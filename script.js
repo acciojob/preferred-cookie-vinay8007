d72871bbc69890e81a8c9276ea8a629b3ba5fef3
vinay8007 <161361880+vinay8007@users.noreply.github.com>
2025-11-29 19:52:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-//your JS code here. If required.
+function getCoockie(name){
+    let decoded = decodeURIComponent(document.cookie);
+    let parts = decoded.split(", ");
+    for(let part of parts){
+        let [key, value] = part.split("=");
+        if (key === name) return value;
+    }
+    return null;
+}
+
+window.onload = function(){
+    let savedSize = getCoockie("fontSize");
+    let savedColor = getCoockie("fontColor");
+
+    if(savedSize){
+        document.documentElement.style.setProperty("--fontsize", savedSize + "px");
+        document.getElementById("fontsize").value = savedSize;
+    }
+
+    
+    if(savedColor){
+        document.documentElement.style.setProperty("--fontsize", savedColor + "px");
+        document.getElementById("fontsize").value = savedColor;
+    }
+};
+
+document.getElementById("settingsForm").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    let size = document.getElementById("fontsize").value;
+    let color = document.getElementById("fontcolor").value;
+
+    document.cookie = "fontsize" + size + "; path=/";
+    document.cookie = "fontcolor" + size + "; path=/";
+
+    alert("preferences saved!");
+});
